ebee0904de0d9311dcaf50c2496e62fbfb80c163
Andrea Crotti <kerny404@gmail.com>
2009-12-18 13:58:36 +0100

    options redefined

--- push_data.py
+++ push_data.py
@@ -1,66 +1,75 @@
 #!/usr/bin/env python
-# Time-stamp: <17-12-2009, 15:20>
+# Time-stamp: <18-12-2009, 13:40>
+# TODO: put all the single files together in only one sync
 
 import os
 #import logging
+#import subprocess
+
 from optparse import OptionParser
-from sys import exit
+from sys import exit, stdout
 
 HOME = os.path.expanduser("~")
 
 RSYNC = "/usr/bin/env rsync"
 # TODO: make it more general
-FILES = open('files.txt').read().split('\n')[:-1]
-
 EXCLUDE = os.path.expanduser("~/bin/exclude_list")
-OPTIONS = ["--exclude-from=" + EXCLUDE, "-az", "--relative"]
+FILES = os.path.expanduser("~/bin/files.txt")
+
+FILELIST = open(FILES).read().split('\n')[:-1]
 
 CMD = RSYNC + " %(opts)s %(src)s %(dst)s"
 
-def sync_files(dst, confirm = False):
+def sync_files(dst, options, confirm = True):
     "Synchronize the file calling rsync with the global OPTIONS"
     os.chdir(HOME)
     dic = {}
-    for f in FILES:
+    for f in FILELIST:
         dic[f] = []
         if os.path.isdir(f):
             dic[f].append("--delete-after")
     for d in dic.iterkeys():
-        cmd = CMD % {'opts' : ' '.join(OPTIONS + dic[d]),
+        cmd = CMD % {'opts' : ' '.join(options + dic[d]),
                      'src' : d, 'dst' : dst + ":"}
-        if not(confirm):
-            while True:
-                r = raw_input("executing %s, confirm? y/n\n" % cmd)
-                if r == 'y':
-                    break
-                elif r == 'n':
-                    exit(0)
-        _, output = os.popen2(cmd)
-        print output.read()
+        sync = True
+        if confirm:
+            r = raw_input("executing %s, confirm? y/n\n" % cmd)
+            if r != 'y':
+                sync = False
+                print "skipping %s from sync" % d
+
+        if sync:
+            print "running command %s" % cmd
+        # by default is already going to stdout
+        # TODO: make it possible to log somewhere the stderr
+        _, out = os.popen2(cmd)
+        print out.read()
 
 if __name__ == '__main__':
     # FIXME: not really nice use of global
-    global OPTIONS
     opt_parser = OptionParser(usage = "push_data.py -o <additional options> [-p] user@host")
     opt_parser.add_option("-v", "--verbose", action = "store_true", dest = "verbose")
     opt_parser.add_option("-p", "--pretend", action = "store_true", dest = "pretend",
                           help = "just pretend don't do")
-    opt_parser.add_option("-y", "--no-confirm", action = "store_true",
+    opt_parser.add_option("-y", "--no-confirm", action = "store_false",
                           dest = "confirm", help = "don't demand confirmation")
-    (options, args) = opt_parser.parse_args()
+    (opts, args) = opt_parser.parse_args()
 
-    if options.pretend:
-        OPTIONS.append("-n")
+    options = ["--exclude-from=" + EXCLUDE, "-az", "--relative"]
+
+    if opts.pretend:
+        options.append("-n")
         
-    if options.verbose:
-        OPTIONS.append("-v")
+    if opts.verbose:
+        options.append("-v")
         
     # using the correct 
     if not args:
         exit(os.EX_NOINPUT)
     
+    # TODO: make it able to run in parallel
     # this can also send on more hosts at the same time!
     # maybe should be better if running in parallel?
     for host in args:
-        sync_files(host, confirm = options.confirm)
+        sync_files(host, options, confirm = opts.confirm)
     
