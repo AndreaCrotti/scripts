4cf40fae2f925df7b054bfec9a8a52ca1afc0a65
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-12-22 20:06:31 +0100

    simplify the extension list

--- backup.py
+++ backup.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 import yaml
 
+"""
+Two main possible destination type
+- backup disk
+- other host
+
+rsync can be used in both cases unless the destination disk is in fat32.
+for saving some space we can use git clone whenever we are backing up a git repository
+"""
+
 conf = yaml.load(open("backup.yaml"))
 
 if __name__ == '__main__':
