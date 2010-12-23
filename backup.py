52476f0454d635f64a66be25a41fada42a9eab1a
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-12-23 12:24:32 +0100

    shell command class

--- backup.py
+++ backup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import yaml
 
 """
 Two main possible destination type
@@ -10,7 +9,37 @@ rsync can be used in both cases unless the destination disk is in fat32.
 for saving some space we can use git clone whenever we are backing up a git repository
 """
 
+import yaml
+import subprocess # for the call process
+
 conf = yaml.load(open("backup.yaml"))
+
+# should only generate one huge rsync command
+
+HOME = os.path.expanduser("~")
+
+class ShellCommand(object):
+    def __init__(self, base=HOME, verbose=False):
+        self.verbose = verbose
+        # base path for the command, also from where it should be executed
+        self.base = base
+
+    def __str__(self):
+        return " ".join(self.cmd)
+
+    def execute(self):
+        pass
+
+
+class RsyncCommand(object):
+    def __init__(self, verbose):
+        # a command is a list of options, arguments etc etc
+        self.cmd = []
+
+    def __setattribute__(self, attr, value):
+        self.cmd.append((attr, value))
 
 if __name__ == '__main__':
     print conf
+    RsyncCommand(cmd, base="/usr")
+    
