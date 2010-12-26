ab383fbbfb1fe35cc3fb89ed58d3224a0ebc3c49
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-12-26 03:42:21 +0100

    start the test on the external drive. RdiffCmd works well but Git command still needs to be fixed

--- backup.py
+++ backup.py
@@ -7,6 +7,9 @@ Two main possible destination type
 
 rsync can be used in both cases unless the destination disk is in fat32.
 for saving some space we can use git clone whenever we are backing up a git repository
+
+TODO:
+- should be possible to confirm after seeing some statistics
 """
 
 import yaml
@@ -38,30 +41,43 @@ class ShellCommand(object):
     def add(self, attr):
         self.cmd.append(attr)
 
-    def execute(self):
+    def run(self):
+        print "executing %s" % str(self)
         # TODO: check the return code and parallelize when possible 
         subprocess.Popen(str(self), shell=True)
 
 
 class RdiffCommand(ShellCommand):
-    " Rdiff commands"
-    def __init__(self, base=HOME):
+    def __init__(self, source, opts=None, base=HOME):
         # a command is a list of options, arguments etc etc
         super(RdiffCommand, self).__init__(base)
-        self.cmd.append(CMD % "rdiff-backup")
+        self.add(CMD % "rdiff-backup")
+        if opts is not None:
+            self.add(opts)
 
-    def exclude_list(self, exts):
+        self.source = source
+        self.add(source)
+
+    # exclude list should be one global and possibly many local
+    @staticmethod
+    def exclude_list(exts):
         return '|'.join([x + '$' for x in exts])
 
+    def execute(self, dest):
+        chdir(HOME)
+        self.add(path.join(dest, self.source.split("/")[-1]))
+        super(RdiffCommand, self).run()
+        
 
 class GitCommand(ShellCommand):
-    def __init__(self, base=HOME, repo=None):
+    def __init__(self, repo, base=HOME):
         super(GitCommand, self).__init__(base)
         # if the destination file already exists we should use git pull instead, much faster
+        # TODO: the repo name should be passed like this from the external call 
         self.repo = repo.split("/")[-1]
 
     def execute(self, dest):
-        from os import listdir, getcwd
+        # TODO: use context manager for this context changes 
         chdir(dest)
         if path.exists(self.repo):
             chdir(self.repo)
@@ -69,12 +85,13 @@ class GitCommand(ShellCommand):
         else:
             self.add(CMD % ("git clone %s" % self.repo))
 
-        super(GitCommand, self).execute()
+        super(GitCommand, self).run()
 
 
 def backup(dest):
     # keep a list to make them run in parallel as much as possible
     commands = []
+    glob_exclude = '--exclude-regexp="%s"' % RdiffCommand.exclude_list(conf['exclude_ext'])
     # read the configuration how to do it
     for s in conf['sources']:
         if isinstance(s, dict):
@@ -83,19 +100,14 @@ def backup(dest):
             subkey = s.keys()[0]
             for subdir, repo in s[subkey].items():
                 if repo == 'git':
-                    gt = GitCommand(repo=subdir)
+                    gt = GitCommand(subdir)
                     commands.append(gt)
 
-        # elif isinstance(s, str):
-        #     # pass the right verbosity flags and so on
-        #     rd = RdiffCommand()
-        #     # depending on the type of connection we should do different things
-        #     # rd += 
-        #     # now add the right arguments
-        #     commands.append(rd)
+        elif isinstance(s, str):
+            rd = RdiffCommand(s, opts=glob_exclude)
+            commands.append(rd)
 
     for cmd in commands:
-        # print cmd
         cmd.execute(conf['destinations'][dest]['path'])
 
 
@@ -113,5 +125,5 @@ if __name__ == '__main__':
 
     # assert(destination is not None)
 
-    backup('test')
+    backup('backup')
     
