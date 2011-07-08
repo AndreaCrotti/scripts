fa363255d8cf3a666401aba062113ce20ac501e6
Andrea Crotti <andrea.crotti.0@gmail.com>
2011-07-08 13:02:29 +0200

    refactored c_gprof code

--- c_gprof.sh
+++ c_gprof.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
-# change those variables as you prefer
 
-if [ $# -lt 2 ]
-then 
-    echo "usage $0 <program> [arguments...]"
-    exit 1
-fi
+# TODO: add some trapping mechanism to catch errors
 
 PROG=$1
 PROF="gmon.out"
 TYPE=pdf
 OUT=profiled.$TYPE
+
+if [ $# -lt 1 ]
+then 
+    echo "usage $0 <program> [arguments...]"
+    exit 1
+fi
+
    
 if uname | grep -i 'darwin'
 then
     GPROF2DOT="gprof2dot.py"
-elif uname | grep -i 'linux'
-then
+else
     GPROF2DOT="gprof2dot"
 fi
 
-set -x
-
 ./$@
 gprof $PROG | $GPROF2DOT -e0 -n0 | dot -T$TYPE -o $OUT
-echo "now open $OUT"
+echo "graph in $OUT"
