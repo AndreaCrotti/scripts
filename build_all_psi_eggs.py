cb74d08b62f31e52a71ba17056575e3b0adf7b86
Andrea Crotti <andrea.crotti.0@gmail.com>
2011-10-11 13:47:47 +0100

    develop and build many eggs from a directory

---
+++ build_all_psi_eggs.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python2
+
+"""
+Build all the PSI eggs with develop or bdist_egg depending on the case
+"""
+
+from os import getenv, walk, path
+from subprocess import Popen, PIPE
+from glob import glob
+import argparse
+
+PSI_PATH = getenv("PSI_PATH")
+EGG_DIR = path.expanduser(path.join('~', '.psi_eggs'))
+# where is the server running
+SERVER_LINK = "http://localhost:8080"
+
+# glob on the paths without using chdir, but using the absolute paths
+
+# use os.walk to scan through the needed eggs, to rebuild them
+# automatically
+
+# TODO: just create the bdist_egg and make sure that the local pypi
+# server is actually running, then "develop" will actually be easier
+# to solve
+def analyzer(option):
+    # get the return value with subprocess
+    eggs = glob(path.join(PSI_PATH, '*'))
+    if option == 'list':
+        print(str(eggs))
+        return None
+
+    # XXX: will keep going forever in case something also the second
+    # time is not working
+    cmd = "python2 setup.py %s" % option
+    while eggs:
+        egg = eggs.pop()
+        # TODO: filter in a smarter way possibly
+        if not path.exists(path.join(egg, "setup.py")):
+            print("filtering out egg %s" % egg)
+            continue
+        # change the directory before running
+        p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, cwd=egg)
+        out, err = p.communicate()
+
+        # TODO: make it as parallel as possible
+        if p.returncode != 0:
+            print("setup on %s failed, add it again to the list" % egg)
+            eggs.insert(0, egg)
+            print(err)
+
+        
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='script to run things')
+    parser.add_argument('-b', '--build',
+                        help='build the binary eggs',
+                        action='store_true')
+    parser.add_argument('-d', '--develop',
+                        help='develop all the eggs',
+                        action='store_true')
+    parser.add_argument('-l', '--list', action='store_true')
+    
+    #TODO: make sure we do either one or the other one
+    ns = parser.parse_args()
+
+    if ns.build:
+        option = 'bdist_egg -d %s' % EGG_DIR
+    elif ns.develop:
+        option = 'develop --user'
+    else:
+        print("only showing the given eggs")
+        option = 'list'
+
+    analyzer(option)
