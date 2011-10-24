16b251ebf1a2e43a197952a44c3db49d7bd7c762
Andrea Crotti <andrea.crotti.0@gmail.com>
2011-10-24 16:07:51 +0100

    add more possible skeleton for the build / service program

--- build_all_psi_eggs.py
+++ build_all_psi_eggs.py
@@ -1,3 +1,4 @@
+#!/home/andrea/python_pack/airbus_epd-2.5.2001.b1-rh3_amd64/bin/python
 """
 Build all the PSI eggs with develop or bdist_egg depending on the case
 """
@@ -5,17 +6,33 @@ Build all the PSI eggs with develop or bdist_egg depending on the case
 from os import getenv, walk, path
 from subprocess import Popen, PIPE
 from glob import glob
+# add also this to the third party as egg
 import argparse
 
+from pypiserver import core
+# TODO: next step is to create a virtual environment (if not existing
+# already), otherwise just activate it
+from virtualenv import create_environment
+
 PSI_PATH = getenv("PSI_PATH")
+BASE_ENV_PATH = path.expanduser(path.join('~', '.virtualenvs'))
 EGG_DIR = path.expanduser(path.join('~', '.psi_eggs'))
 # where is the server running
 SERVER_LINK = "http://localhost:8080"
+PYTHON_CMD = path.expanduser('~/python_pack/airbus_epd-2.5.2001.b1-rh3_amd64/bin/python')
+SERVER_PORT = "8080"
 
 # glob on the paths without using chdir, but using the absolute paths
 
 # use os.walk to scan through the needed eggs, to rebuild them
 # automatically
+
+def switch_to_environment(project):
+    if not path.exists(project):
+        create_environment(project)
+    # activation is not really necessary, or is it?
+    # now we should switch automatically to the interpreter given in the virtual env
+    
 
 # TODO: just create the bdist_egg and make sure that the local pypi
 # server is actually running, then "develop" will actually be easier
@@ -29,7 +46,7 @@ def analyzer(option):
 
     # XXX: will keep going forever in case something also the second
     # time is not working
-    cmd = "python2 setup.py %s" % option
+    cmd = "%s setup.py %s" % (PYTHON_CMD, option)
     while eggs:
         egg = eggs.pop()
         # TODO: filter in a smarter way possibly
@@ -52,9 +69,11 @@ if __name__ == '__main__':
     parser.add_argument('-b', '--build',
                         help='build the binary eggs',
                         action='store_true')
+
     parser.add_argument('-d', '--develop',
                         help='develop all the eggs',
                         action='store_true')
+
     parser.add_argument('-l', '--list', action='store_true')
     
     #TODO: make sure we do either one or the other one
