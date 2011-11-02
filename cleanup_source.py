7713337bf6157e48b7c4027097f99641bb97ca9d
Andrea Crotti <andrea.crotti.0@gmail.com>
2011-11-02 10:44:44 +0000

    use many global variables to avoid passing in arguments

--- cleanup_source.py
+++ cleanup_source.py
@@ -4,24 +4,25 @@ from sys import argv, exit
 import logging
 import argparse
 
-TO_REMOVE_DEFAULT = "# -*-mode: python"
-
-DEFAULT_EXTENSION = ".py"
+#TODO: remove empty newlines after this too
+TO_REMOVE = "# -*-mode: python"
+EXTENSION = ".py"
+SIMULATE = False
 
 # TODO: make TO_REMOVE also a parameter
-def remove_comment(arg, dirname, fnames, modify=False):
+def remove_comment(arg, dirname, fnames):
     # we can do some smart filtering based on the extension
     def rewrite_if_matching(fname):
         lines = open(fname).readlines()
 
-        if lines and lines[0].startswith(TO_REMOVE_DEFAULT):
+        if lines and lines[0].startswith(TO_REMOVE):
             print("removing from %s line %s" % (fname, lines[0]))
-            if modify:
+            if not SIMULATE:
                 print("really modifying things")
-                # open(fname, 'w').writelines(lines[1:])
+                open(fname, 'w').writelines(lines[1:])
 
     for f in fnames:
-        if f.endswith(DEFAULT_EXTENSION):
+        if f.endswith(EXTENSION):
             full = path.join(dirname, f)
             #  FIXME: why do I need to check if it's a file
             if path.isfile(full):
@@ -38,5 +39,18 @@ if __name__ == '__main__':
     parser.add_argument('-e', '--ext', default=DEFAULT_EXTENSION)
 
     args = parser.parse_args()
+
+    if args.simulate:
+        global SIMULATE
+        SIMULATE = True
+
+    if args.ext:
+        global EXTENSION
+        EXTENSION = True
+
+    if args.regexp:
+        global TO_REMOVE
+        TO_REMOVE = args.regexp
+
     for d in args.dest:
         path.walk(d, remove_comment, None)
