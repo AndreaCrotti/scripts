9299547ab398eaa213aab4c880e2701c866ccd71
Andrea Crotti <andrea.crotti.0@gmail.com>
2011-11-26 19:41:24 +0000

    use the ast instead of the deprecated compiler module

--- analyze_imports.py
+++ analyze_imports.py
@@ -2,83 +2,31 @@
 
 import argparse
 
-from compiler import parseFile, walk
-from compiler.visitor import ASTVisitor
+from ast import parse, NodeVisitor
 
-# now we can define a visitor
-class ImportVisitor(object):
-    """AST visitor for grabbing the import statements.
 
-    This visitor produces a list of
+class ImportVisitor(NodeVisitor):
 
-       (module-name, remote-name, local-name, line-no, pragma)
-
-    * remote-name is the name off the symbol in the imported module.
-    * local-name is the name of the object given in the importing module.
-    """
     def __init__(self):
-        self.modules = []
-        self.recent = []
+        self.imported = []
+        super(ImportVisitor, self).__init__()
 
-    def visitImport(self, node):
-        self.accept_imports()
-        self.recent.extend((x[0], None, x[1] or x[0], node.lineno, 0)
-                           for x in node.names)
+    def __str__(self):
+        return str(self.imported)        
 
-    def visitFrom(self, node):
-        self.accept_imports()
-        modname = node.modname
-        if modname == '__future__':
-            return # Ignore these.
-        for name, as_ in node.names:
-            if name == '*':
-                # We really don't know...
-                mod = (modname, None, None, node.lineno, node.level)
-            else:
-                mod = (modname, name, as_ or name, node.lineno, node.level)
-            self.recent.append(mod)
+    def visit_Import(self, node):
+        for n in node.names:
+            self.imported.append(n.name)
 
-    def default(self, node):
-        pragma = None
-        if self.recent:
-            if isinstance(node, Discard):
-                children = node.getChildren()
-                if len(children) == 1 and isinstance(children[0], Const):
-                    const_node = children[0]
-                    pragma = const_node.value
+    def visit_ImportFrom(self, node):
+        self.imported.append(node.module)
 
-        self.accept_imports(pragma)
-
-    def accept_imports(self, pragma=None):
-        self.modules.extend((m, r, l, n, lvl, pragma)
-                            for (m, r, l, n, lvl) in self.recent)
-        self.recent = []
-
-    def finalize(self):
-        self.accept_imports()
-        return self.modules
-
-
-class ImportWalker(ASTVisitor):
-    "AST walker that we use to dispatch to a default method on the visitor."
-
-    def __init__(self, visitor):
-        ASTVisitor.__init__(self)
-        self._visitor = visitor
-
-    def default(self, node, *args):
-        self._visitor.default(node)
-        ASTVisitor.default(self, node, *args)
 
 def show_imports(mod):
-    ast = parseFile(mod)
-    # for n in ast.node.nodes:
-    #     print(n)
-
-    vis = ImportVisitor()
-    walk(ast, vis, ImportWalker(vis))
-    found_imports = vis.finalize()
-    print(found_imports)
+    at = parse(open(mod).read())
+    v = ImportVisitor()
+    v.visit(at)
+    print(v)
 
 
 def parse_arguments():
