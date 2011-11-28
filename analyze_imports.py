0d918baea0be395ce9cbdc651b8bf103ec162ee3
Andrea Crotti <andrea.crotti.0@gmail.com>
2011-11-28 11:17:16 +0000

    make the analyze import script works nicely

--- analyze_imports.py
+++ analyze_imports.py
@@ -3,6 +3,10 @@
 import argparse
 
 from ast import parse, NodeVisitor
+from os import path
+from pprint import pprint
+
+from psi.devsonly.walk import walk_py, walk_full_path_files
 
 
 class ImportVisitor(NodeVisitor):
@@ -12,32 +16,48 @@ class ImportVisitor(NodeVisitor):
         super(ImportVisitor, self).__init__()
 
     def __str__(self):
-        return str(self.imported)        
+        return '\n'.join(x for x in self.imported)
 
     #TODO: maybe I should make a difference between these two cases?
     def visit_Import(self, node):
         for n in node.names:
             self.imported.add(n.name)
 
+    #TODO: store something more than simply the name of the module
+    #that we are using
     def visit_ImportFrom(self, node):
         self.imported.add(node.module)
 
 
-def show_imports(mod):
-    at = parse(open(mod).read())
-    v = ImportVisitor()
-    v.visit(at)
-    print v
+def gen_module_imports(mod):
+    try:
+        at = parse(open(mod).read())
+    except SyntaxError:
+        print("file %s has a syntax error, please fix it" % mod)
+        return []
+    else:
+        v = ImportVisitor()
+        v.visit(at)
+        return v.imported
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='analyze the imports and print them out')
-    
-    parser.add_argument('modules', nargs='+')
+    # we can get a list of directories, files, or mix
+    parser.add_argument('cont', nargs='+')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     ns = parse_arguments()
-    for mod in ns.modules:
-        show_imports(mod)
+    imports = {}
+
+    for cn in ns.cont:
+        if path.isfile(path.abspath(cn)):
+            imports[cn] = gen_module_imports(cn)
+
+        elif path.isdir(path.abspath(cn)):
+            for mod in walk_full_path_files(cn, walk_py):
+                imports[mod] = gen_module_imports(mod)
+
+    pprint(imports)
