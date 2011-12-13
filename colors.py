4444f755d9f15d077d8262eaac6227bcfbbbda84
Andrea Crotti <andrea.crotti.0@gmail.com>
2011-12-13 21:16:30 +0000

    nice random printer

--- colors.py
+++ colors.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python2
 
+import random
+from string import ascii_letters
+
 COLORS = map(str, range(31,38))
 RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = COLORS
 
@@ -15,6 +18,12 @@ def next_color(color):
     return COLORS[(COLORS.index(color) + 1) % len(COLORS)]
 
 
-m = "message"
-for c in COLORS:
-    print(colorize_string(c, m))
+def random_printer(dim):
+    for i in range(dim):
+        if random.random() > 0.1:
+            l = random.choice(ascii_letters)
+            print(colorize_string(random.choice(COLORS), l)),
+        else:
+            print(" "),
+
+random_printer(100 * 100 * 10)
