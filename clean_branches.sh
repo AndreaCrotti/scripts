1c922928300288f597132a8228b83cc71cbd7014
Andrea Crotti <andrea.crotti.0@gmail.com>
2016-05-05 10:33:47 +0100

    avoid master itself and make the script executable

--- clean_branches.sh
+++ clean_branches.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-for branch in $(git branch --merged master); do git branch -d $branch; done
+for branch in $(git branch --merged master | grep -v master)
+do
+    git branch -d $branch
+done
