c7925d98437e2c6ce8f61b1853493d1b8483ea45
Andrea Crotti <andrea.crotti.0@gmail.com>
2025-01-21 17:52:56 +0000

    cleanup

--- cloc.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cloc ./ --unix --by-file --csv --quiet $*
--- run_kafka_ui.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/env bash
-
-docker run -p 8080:8080 \
-	-e KAFKA_CLUSTERS_0_NAME=local \
-	-e KAFKA_CLUSTERS_0_BOOTSTRAPSERVERS=kafka:9092 \
-	-d provectuslabs/kafka-ui:latest
